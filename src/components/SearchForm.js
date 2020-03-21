2eed26cf92a7719a034c013cae146fc8fc903ac9
AustinJHealy <56621614+AustinJHealy@users.noreply.github.com>
2020-03-20 20:42:38 -0400

    rendered character names and added search form without functionality

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,25 @@
 import React, { useState } from "react";
 
-export default function SearchForm() {
  
+export default function SearchForm({ onSearch }) {
+  const [query, setQuery] = useState({
+    name: ""
+  })
+  const changeHandler = (event) => {
+    setQuery({ ...query, name: event.target.value })
+  }
+
   return (
     <section className="search-form">
-     // Add a search form here
+     <form onSubmit={() => onSearch(query)}>
+        <input
+          onChange={changeHandler}
+          placeholder="name"
+          value={query.name}
+          name="name"
+        />
+        <button type="submit">Search</button>
+      </form>
     </section>
   );
 }
